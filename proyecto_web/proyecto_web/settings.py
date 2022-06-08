f85e9fb9c77e57bbe7fa017e033bbe3fe0ef9a3c
El-gran-patriarca <ju.buitrago@duocuc.cl>
2022-06-08 19:48:36 -0400

    Implementacion de base de datos

--- proyecto_web/proyecto_web/settings.py
+++ proyecto_web/proyecto_web/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'proyecto_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'c##proyecto_web',
+        'PASSWORD': 'ja_fa',
+        'TEST':{
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 
