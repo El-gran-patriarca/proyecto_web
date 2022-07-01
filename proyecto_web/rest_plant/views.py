ecbaffa285af8d3416182b16093143a02d524109
Lancelott3 <fran.gonzalezs@duocuc.cl>
2022-07-01 00:44:24 -0400

    Avance Resgistro

--- proyecto_web/rest_plant/views.py
+++ proyecto_web/rest_plant/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from core.models import Plantas
 from .serializers import PlantasSerializer
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 @csrf_exempt
 @api_view(['GET', 'POST'])
+#@permission_classes((IsAuthenticated,))
 def lista_plantas(request):
 
     #Lista todas las plantas
@@ -27,6 +31,7 @@ def lista_plantas(request):
 
 @csrf_exempt
 @api_view(['GET', 'PUT', 'DELETE'])
+#@permission_classes((IsAuthenticated,))
 def detalle_plantas(request, id):
 
     #get, update y delete 
